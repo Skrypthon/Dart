6ebb852bdf16e5e419b2b8d6973b1941742fb80d
HernanB <jesusbonillagar@gmail.com>
2022-04-20 00:28:03 -0500

    inicizalización proyecto en Dart

---
+++ aplicacion_dart/bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:aplicacion_dart/aplicacion_dart.dart' as aplicacion_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aplicacion_dart.calculate()}!');
+  print('this is a new line');
+}
